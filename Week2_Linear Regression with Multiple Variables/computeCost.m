ba616066224b474d76b6776357f9cdcca301da02
emmanguyen102 <57014399+emmanguyen102@users.noreply.github.com>
2020-05-09 17:07:21 +0300

    Add files via upload

---
+++ Week2_Linear Regression with Multiple Variables/computeCost.m
@@ -0,0 +1,17 @@
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+% Initialize some useful values
+m = size(X,1); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+
+predictions = X*theta; %predictions of hypothesis on all m
+sqrErrors = (predictions-y).^2;
+
+J =1/(2*m) * sum(sqrErrors);
+
+end
